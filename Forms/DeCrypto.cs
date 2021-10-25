ecae62a48aef2ec3fdd01329615acf154c72a366
Salih Duruk <mehmet00salih00@gmail.com>
2021-10-25 19:52:57 +0300

    25.10

--- Forms/DeCrypto.cs
+++ Forms/DeCrypto.cs
@@ -24,17 +24,14 @@ namespace AESCrypto.Forms
         public string DosyadanOku(string dosya_yolu)
         {
             FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
-            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
-            //2.parametre dosyanın açılacağını,
-            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            
             StreamReader sw = new StreamReader(fs);
-            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            
             string yazi = sw.ReadLine();
-            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
-            //Son satır okunduktan sonra okuma işlemini bitirdik
+            
             sw.Close();
             fs.Close();
-            //İşimiz bitince kullandığımız nesneleri iade ettik.
+            
             return yazi;
         }
         private void BtnSifreCoz_Click(object sender, EventArgs e)
